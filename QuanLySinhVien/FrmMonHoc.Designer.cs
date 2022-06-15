8a7b0f5ad24a0c6a33701fbf0fbcf64f23cafe4c
sas0125 <khacthanhsang@gmail.com>
2022-06-15 09:20:12 +0700

    Sửa forms

--- QuanLySinhVien/FrmMonHoc.Designer.cs
+++ QuanLySinhVien/FrmMonHoc.Designer.cs
@@ -29,102 +29,126 @@ namespace QuanLySinhVien
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMonHoc));
             this.cbKhoa = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnXem = new System.Windows.Forms.Button();
             this.dgvMon = new System.Windows.Forms.DataGridView();
-            this.btnThoat = new System.Windows.Forms.Button();
             this.ColMaMon = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColTenMon = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColSoTC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnThoat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMon)).BeginInit();
             this.SuspendLayout();
             // 
             // cbKhoa
             // 
             this.cbKhoa.FormattingEnabled = true;
-            this.cbKhoa.Location = new System.Drawing.Point(117, 23);
+            this.cbKhoa.Location = new System.Drawing.Point(156, 28);
+            this.cbKhoa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbKhoa.Name = "cbKhoa";
-            this.cbKhoa.Size = new System.Drawing.Size(121, 21);
+            this.cbKhoa.Size = new System.Drawing.Size(160, 24);
             this.cbKhoa.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(117, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Chọn khoa";
             // 
             // btnXem
             // 
-            this.btnXem.Location = new System.Drawing.Point(277, 21);
+            this.btnXem.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnXem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.Location = new System.Drawing.Point(369, 26);
+            this.btnXem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnXem.Name = "btnXem";
-            this.btnXem.Size = new System.Drawing.Size(75, 23);
+            this.btnXem.Size = new System.Drawing.Size(100, 34);
             this.btnXem.TabIndex = 2;
             this.btnXem.Text = "Xem";
-            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.UseVisualStyleBackColor = false;
             this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
             // 
             // dgvMon
             // 
             this.dgvMon.AllowUserToDeleteRows = false;
+            this.dgvMon.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvMon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColMaMon,
             this.ColTenMon,
             this.ColSoTC});
-            this.dgvMon.Location = new System.Drawing.Point(29, 55);
+            this.dgvMon.Location = new System.Drawing.Point(39, 68);
+            this.dgvMon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvMon.Name = "dgvMon";
             this.dgvMon.ReadOnly = true;
-            this.dgvMon.Size = new System.Drawing.Size(323, 187);
+            this.dgvMon.RowHeadersWidth = 51;
+            this.dgvMon.Size = new System.Drawing.Size(431, 230);
             this.dgvMon.TabIndex = 3;
-            // 
-            // btnThoat
-            // 
-            this.btnThoat.Location = new System.Drawing.Point(277, 262);
-            this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 23);
-            this.btnThoat.TabIndex = 4;
-            this.btnThoat.Text = "Thoát";
-            this.btnThoat.UseVisualStyleBackColor = true;
-            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // ColMaMon
             // 
             this.ColMaMon.DataPropertyName = "MaMon";
             this.ColMaMon.HeaderText = "Mã môn";
+            this.ColMaMon.MinimumWidth = 6;
             this.ColMaMon.Name = "ColMaMon";
             this.ColMaMon.ReadOnly = true;
+            this.ColMaMon.Width = 125;
             // 
             // ColTenMon
             // 
             this.ColTenMon.DataPropertyName = "TenMon";
             this.ColTenMon.HeaderText = "Tên môn";
+            this.ColTenMon.MinimumWidth = 6;
             this.ColTenMon.Name = "ColTenMon";
             this.ColTenMon.ReadOnly = true;
+            this.ColTenMon.Width = 125;
             // 
             // ColSoTC
             // 
             this.ColSoTC.DataPropertyName = "SoTinChi";
             this.ColSoTC.HeaderText = "Số tín chỉ";
+            this.ColSoTC.MinimumWidth = 6;
             this.ColSoTC.Name = "ColSoTC";
             this.ColSoTC.ReadOnly = true;
+            this.ColSoTC.Width = 125;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Crimson;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(350, 322);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(119, 31);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // FrmMonHoc
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 297);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(512, 366);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.dgvMon);
             this.Controls.Add(this.btnXem);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbKhoa);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmMonHoc";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Môn học";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FrmMonHoc_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMon)).EndInit();
             this.ResumeLayout(false);
--- QuanLySinhVien/FrmSinhVien_GV.Designer.cs
+++ QuanLySinhVien/FrmSinhVien_GV.Designer.cs
@@ -29,37 +29,44 @@ namespace QuanLySinhVien
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSinhVien_GV));
             this.txbTim = new System.Windows.Forms.TextBox();
             this.btnTim = new System.Windows.Forms.Button();
             this.dgvSV = new System.Windows.Forms.DataGridView();
-            this.btnThoat = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnThoat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSV)).BeginInit();
             this.SuspendLayout();
             // 
             // txbTim
             // 
-            this.txbTim.Location = new System.Drawing.Point(28, 14);
+            this.txbTim.Location = new System.Drawing.Point(37, 26);
+            this.txbTim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txbTim.Name = "txbTim";
-            this.txbTim.Size = new System.Drawing.Size(221, 20);
+            this.txbTim.Size = new System.Drawing.Size(293, 22);
             this.txbTim.TabIndex = 0;
             // 
             // btnTim
             // 
-            this.btnTim.Location = new System.Drawing.Point(291, 12);
+            this.btnTim.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTim.Location = new System.Drawing.Point(369, 18);
+            this.btnTim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnTim.Name = "btnTim";
-            this.btnTim.Size = new System.Drawing.Size(75, 23);
+            this.btnTim.Size = new System.Drawing.Size(154, 33);
             this.btnTim.TabIndex = 1;
             this.btnTim.Text = "Tìm kiếm";
-            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.UseVisualStyleBackColor = false;
             this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
             // 
             // dgvSV
             // 
+            this.dgvSV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvSV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -67,68 +74,87 @@ namespace QuanLySinhVien
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dgvSV.Location = new System.Drawing.Point(28, 55);
+            this.dgvSV.Location = new System.Drawing.Point(37, 68);
+            this.dgvSV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvSV.Name = "dgvSV";
-            this.dgvSV.Size = new System.Drawing.Size(666, 323);
+            this.dgvSV.RowHeadersWidth = 51;
+            this.dgvSV.Size = new System.Drawing.Size(888, 398);
             this.dgvSV.TabIndex = 2;
-            // 
-            // btnThoat
-            // 
-            this.btnThoat.Location = new System.Drawing.Point(619, 402);
-            this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 23);
-            this.btnThoat.TabIndex = 3;
-            this.btnThoat.Text = "Thoát";
-            this.btnThoat.UseVisualStyleBackColor = true;
-            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // Column1
             // 
             this.Column1.DataPropertyName = "MaSV";
             this.Column1.HeaderText = "Mã số sinh viên";
+            this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
             this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
             // 
             // Column2
             // 
             this.Column2.DataPropertyName = "HoTen";
             this.Column2.HeaderText = "Họ tên";
+            this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
             this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
             // 
             // Column3
             // 
             this.Column3.DataPropertyName = "GioiTinh";
             this.Column3.HeaderText = "Nữ";
+            this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
             this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
             // 
             // Column4
             // 
             this.Column4.DataPropertyName = "NgaySinh";
             this.Column4.HeaderText = "Ngày sinh";
+            this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
             this.Column4.ReadOnly = true;
+            this.Column4.Width = 125;
             // 
             // Column5
             // 
             this.Column5.DataPropertyName = "MaKhoa";
             this.Column5.HeaderText = "Khoa";
+            this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
             this.Column5.ReadOnly = true;
+            this.Column5.Width = 125;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Crimson;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(787, 484);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(138, 46);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // FrmSinhVien_GV
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(956, 554);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.dgvSV);
             this.Controls.Add(this.btnTim);
             this.Controls.Add(this.txbTim);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmSinhVien_GV";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sinh viên";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FrmSinhVien_GV_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvSV)).EndInit();
             this.ResumeLayout(false);
